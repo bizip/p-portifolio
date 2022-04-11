9876e1297b1e81a399b30b7c8081bc62bf6699c3
bizip <bizip04@gmail.com>
2022-04-11 16:18:19 +0200

    Fix mobile menu functionalitie issue

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 let popupPage = document.querySelector(".popup-page");
-let popupLinks = document.querySelectorAll(".page-popup__link");
 let humbergur = document.querySelector(".pophumbergur");
+let popupLinks = document.querySelectorAll(".page-popup__link");
 let closeButton = document.querySelector(".popup-page__icon");
 function myToggler() {
-  if (popupPage.classList.contains("pop-menu")) {
-    popupPage.classList.remove("pop-menu");
-    humbergur.style.display = "block";
-    popupLinks.style.display = "none";
-    popupPage.style.display = "none";
-    closeButton.style.display = "none";
-  } else {
-    popupPage.classList.add("pop-menu");
-    humbergur.style.display = "none";
-    popupLinks.style.display = "block";
-    popupPage.style.display = "block";
-    closeButton.style.display = "block";
-  }
+    if (popupPage.classList.contains("pop-menu")) {
+        popupPage.classList.remove("pop-menu");
+        humbergur.style.display = "block";
+        popupPage.style.display = "none";
+        closeButton.style.display = "none";
+    } else {
+        popupPage.classList.add("pop-menu");
+        humbergur.style.display = "none";
+        popupPage.style.display = "block";
+        closeButton.style.display = "flex";
+    }
 }
 humbergur.addEventListener("click", myToggler);
 closeButton.addEventListener("click", myToggler);
 popupLinks.forEach((popupPage) => {
-  popupLinks.addEventListener("click", myToggler);
+    popupPage.addEventListener("click", myToggler);
 });
