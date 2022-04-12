c682ac0fa81b8bcbca6490fa51fc40089c3a1baf
bizip <bizip04@gmail.com>
2022-04-12 10:31:15 +0200

    Fix linters error

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-let popupPage = document.querySelector(".popup-page");
-let humbergur = document.querySelector(".pophumbergur");
-let popupLinks = document.querySelectorAll(".page-popup__link");
-let closeButton = document.querySelector(".popup-page__icon");
+const popupPage = document.querySelector('.popup-page');
+const humbergur = document.querySelector('.pophumbergur');
+const popupLinks = document.querySelectorAll('.page-popup__link');
+const closeButton = document.querySelector('.popup-page__icon');
 function myToggler() {
-    if (popupPage.classList.contains("pop-menu")) {
-        popupPage.classList.remove("pop-menu");
-        humbergur.style.display = "block";
-        popupPage.style.display = "none";
-        closeButton.style.display = "none";
-    } else {
-        popupPage.classList.add("pop-menu");
-        humbergur.style.display = "none";
-        popupPage.style.display = "block";
-        closeButton.style.display = "flex";
-    }
+  if (popupPage.classList.contains('pop-menu')) {
+    popupPage.classList.remove('pop-menu');
+    humbergur.style.display = 'block';
+    popupPage.style.display = 'none';
+    closeButton.style.display = 'none';
+  } else {
+    popupPage.classList.add('pop-menu');
+    humbergur.style.display = 'none';
+    popupPage.style.display = 'block';
+    closeButton.style.display = 'flex';
+  }
 }
-humbergur.addEventListener("click", myToggler);
-closeButton.addEventListener("click", myToggler);
+humbergur.addEventListener('click', myToggler);
+closeButton.addEventListener('click', myToggler);
 popupLinks.forEach((popupPage) => {
-    popupPage.addEventListener("click", myToggler);
+  popupPage.addEventListener('click', myToggler);
 });
