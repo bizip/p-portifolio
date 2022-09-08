38536d8759bfe6823ee749b12e6b12636ce7d7cf
bizip <bizip04@gmail.com>
2022-09-08 13:07:42 +0200

    Refacttor the frameword by removing redundancy

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const frm = document.getElementById("frmwork")
 
 const triggerFrameworks = () => {
@@ -10,5 +11,25 @@ const triggerFrameworks = () => {
 
 }
 
+const hider = document.getElementById("tohide")
+let data = fetch('./data/framework_data.json')
+    .then(data => {
+        return data.json();
+    }).then(res => {
+        let results = res;
+        hider.innerHTML = results.map(item =>
+            `
+            <li class="single_frame" id="disp">
+            <div class="image-text">
+              <b>${item.name}</b>
+            </div>
+            <div class="image-list"><img
+                src=${item.image}
+                alt="CSS" />
+            </div>
+            </li>
+            `
+        ).join('');
+    })
 frm.addEventListener("click", triggerFrameworks)
 
